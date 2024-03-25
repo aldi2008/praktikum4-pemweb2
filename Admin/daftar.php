fba29fe4aba737603f123ba7092b99a7d6a6ad29
Aldi <146171912+aldi2008@users.noreply.github.com>
2024-03-25 12:03:25 +0700

    Add files via upload

---
+++ Admin/daftar.php
@@ -0,0 +1,14 @@
+<?php
+include_once './top.php';
+include_once './menu.php';
+?>
+
+<div class="container-fluid px-4">
+    <h1 class="mt-4">Silahkan isi Form Pendaftaran</h1>
+
+    <!-- form daftar -->
+</div>
+
+<?php
+include_once './bottom.php';
+?>
